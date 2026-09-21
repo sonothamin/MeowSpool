db3227fb5b862de2569685539cc9347e06115344
sonothamin <sonothamin.112@gmail.com>
2026-09-21 22:38:05 +0000

    Depend on ui-text-google-fonts for downloadable UI fonts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-text-google-fonts")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
     implementation("com.google.zxing:core:3.5.3")
